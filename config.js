a24ce02d02cf32bec4f3eb3c01dedee1bb475fb1
asta7780 <166767817+asta7780@users.noreply.github.com>
2024-04-17 19:34:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,17 +10,17 @@ global.email = ''
 global.github = 'h'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://653d199bb6634.site123.me/' // add your username
-global.sudo = process.env.SUDO || '201098906252'
-global.devs = '201098906252';
+global.sudo = process.env.SUDO || '967778088098'
+global.devs = '967778088098';
 global.website = 'https://chat.whatsapp.com/H7lNIia7rsaDcYSNWrtTGI' //wa.me/+25000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ab4018d27c9929dc12595.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '*『❄️┇❆ 𝐸𝐿𝑆𝐴 𝐵𝑂𝑇-𝑀𝐷 ❆┇❄️』*' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝐄𝐋𝐆𝐀𝐙𝐀𝐑 ❄️&𝐈𝐓𝐀𝐂𝐇𝐈 👑' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '*𝐒𝐇𝐀𝐃𝐎𝐖*' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𝐒𝐇𝐀𝐃𝐎𝐖' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '*『❄️┇❆ 𝐸𝐿𝑆𝐴 𝐵𝑂𝑇-𝑀𝐷 ❆┇❄️』*' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '*『𝐒𝐇𝐀𝐃𝐎𝐖』*' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '𝐄𝐋𝐆𝐀𝐙𝐀𝐑 ❄️&𝐈𝐓𝐀𝐂𝐇𝐈 👑' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '𝐒𝐇𝐀𝐃𝐎𝐖' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v2' : process.env.VERSION,
-  LANG: process.env.THEME|| 'ELSA',
+  LANG: process.env.THEME|| '𝐒𝐇𝐀𝐃𝐎𝐖',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
